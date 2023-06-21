0b6faa79dbeb6d19b18d05cd18dd8b80883cf157
vitalyvitmens <otm_@tut.by>
2023-06-21 15:25:59 +0300

    add data in project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,25 +3,37 @@ import styles from './app.module.css'
 
 export const App = () => {
 	const [todos, setTodos] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(() => {
+		setIsLoading(true)
+
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedTodo) => {
 				setTodos(loadedTodo)
 			})
+			.finally(() => setIsLoading(false))
 	}, [])
 
 	return (
 		<div className={styles.dataContainer}>
-			{todos.map(({ id, userId, title, completed }) => (
-				<div key={id}>
-					<ol>
-						<h5>{id}</h5>
-						{title}
-					</ol>
-				</div>
-			))}
+			<h3>
+				1. Реализовать приложение на базе Create React App — страницу со списком
+				дел (Todo list):
+			</h3>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				todos.map(({ id, title }) => (
+					<div key={id}>
+						<ol>
+							<span>{id}</span>
+							{title}
+						</ol>
+					</div>
+				))
+			)}
 		</div>
 	)
 }
